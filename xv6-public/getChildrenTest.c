0428565e00efac9dfe94ac9415bf76fd3cca4f69
alireza-pirhadi <alirezapirhadi@gmail.com>
2019-12-20 07:38:28 -0800

    getChildrenTest changed

--- xv6-public/getChildrenTest.c
+++ xv6-public/getChildrenTest.c
@@ -3,19 +3,29 @@
 #include "user.h"
 
 int main(){
+	changePolicy(1);//for avoiding processes to print simultaneously
 	int pid1 = fork();
 	int pid2 = fork();
-	if(pid1 == 0 && pid2 == 0){	
-	printf(1,"pid : %d\n", getpid());
-	printf(1,"	ppid : %d\n", getppid());
-	printf(1,"	childrens : %d\n\n", getChildren(getppid()));
-	exit();	
+	int pid3 = fork();
+	
+	int pid = getpid();
+	int ppid = getppid();
+	int children = getChildren(getppid());
+	printf(1,"pid : %d, ppid : %d, parent's children : %d\n", pid, ppid, children);
+	if(pid1 == 0 && pid2 == 0 && pid3 > 0){
+	    	wait();
 	}
-	else{
-	wait();
-	printf(1,"pid : %d\n", getpid());
-	printf(1,"	ppid : %d\n", getppid());
-	printf(1,"	childrens : %d\n\n", getChildren(getppid()));
+	if(pid1 == 0 && pid2 > 0 && pid3 > 0){
+	    	wait();
+		wait();
+	}
+	if(pid1 > 0 && pid2 == 0 && pid3 > 0){
+	    	wait();
+	}
+	if(pid1 > 0 && pid2 > 0 && pid3 > 0){
+	    	wait();
+		wait();
+		wait();
+	}
 	exit();
-	}
 }
